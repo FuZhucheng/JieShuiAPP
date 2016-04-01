975ed0c31dadd14513114b900bbdbc9109eab466
dasd <751197996@qq.com>
2016-04-01 12:20:50 +0800

    有了TIPS的界面，完善了历史用水界面，但是那个viewpager里面的控件监听跳转出错了intent冲突。WIFI没移植。数据库还没搭好，只写了一个封装类。

--- app/src/main/java/com/baidumap/test/tap/MainActivity.java
+++ app/src/main/java/com/baidumap/test/tap/MainActivity.java
@@ -6,15 +6,18 @@ import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidumap.test.tap.widget.TIPS;
 import com.baidumap.test.tap.widget.XScrollView;
 import com.baidumap.test.tap.widget.dangtianyongshui_activity;
 import com.baidumap.test.tap.widget.lishiyongshui_activity;
@@ -28,6 +31,12 @@ import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -47,9 +56,11 @@ public class MainActivity extends AppCompatActivity implements XScrollView.IXScr
 
     private View content;
     public BarChart barChart;
-    private TextView tv_flow,tv_save,tv_pro,tv_use;
+    private TextView tv_flow,tv_save,tv_pro,tv_use,tv_dianliang;
     public ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+//    public ImageButton img_1,img_2,img_3;        //初始化那个viewpager的view里面的控件。
     public BarDataSet dataset;
+//    public TextView tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9,tv10,tv11,tv12,tv13,tv14,tv15,tv16,tv17,tv18,tv19,tv20,tv21,tv22,tv23,tv24,tv25,tv26,tv27,tv28;
     public ArrayList<String> labels = new ArrayList<String>();
 
 
@@ -67,30 +78,6 @@ public class MainActivity extends AppCompatActivity implements XScrollView.IXScr
         initTips();
 
 
-        //加载viewpager并实现adapter
-        viewPager=(ViewPager)findViewById(R.id.viewPager);
-        //加载viewpager布局图片
-        LayoutInflater lf = getLayoutInflater().from(this);
-        view1 = lf.inflate(R.layout.image1, null);
-        view2 = lf.inflate(R.layout.image2, null);
-        view3 = lf.inflate(R.layout.image3, null);
-
-
-        viewList = new ArrayList<View>();// 将要分页显示的View装入数组中
-        viewList.add(view1);
-        viewList.add(view2);
-        viewList.add(view3);
-
-        //查看更多的监听！！
-        btn_lookmore=(Button)findViewById(R.id.btn_lookmore);
-        btn_lookmore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, lishiyongshui_activity.class);
-                startActivity(intent);
-            }
-        });
         //设置viewPager适配器
         PagerAdapter pagerAdapter = new PagerAdapter() {
 
@@ -148,8 +135,24 @@ public class MainActivity extends AppCompatActivity implements XScrollView.IXScr
         tv_use= (TextView) content.findViewById(R.id.top_use);
         tv_pro= (TextView) content.findViewById(R.id.support_n);
         mScrollView.setView(content);
+        tv_dianliang=(TextView)content.findViewById(R.id.tv_dianliang);
+//        //初始化那个viewpager的view里面的控件。
+//        img_1=(ImageButton)findViewById(R.id.img_1);
+//        img_2=(ImageButton)findViewById(R.id.img_2);
+//        img_3=(ImageButton)findViewById(R.id.img_3);
+        //加载viewpager并实现adapter
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        //加载viewpager布局图片
+        LayoutInflater lf = getLayoutInflater().from(this);
+        view1 = lf.inflate(R.layout.image1, null);
+        view2 = lf.inflate(R.layout.image2, null);
+        view3 = lf.inflate(R.layout.image3, null);
 
 
+        viewList = new ArrayList<View>();// 将要分页显示的View装入数组中
+        viewList.add(view1);
+        viewList.add(view2);
+        viewList.add(view3);
     }
 
     private void initTips() {
@@ -166,6 +169,43 @@ public class MainActivity extends AppCompatActivity implements XScrollView.IXScr
                 Intent intent1=new Intent();
                 intent1.setClass(MainActivity.this,dangtianyongshui_activity.class);
                 startActivity(intent1);
+
+                //查看更多的监听！！跳到历史节水
+                btn_lookmore=(Button)findViewById(R.id.btn_lookmore);
+                btn_lookmore.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this, lishiyongshui_activity.class);
+                        startActivity(intent);
+                    }
+                });
+
+//                //viewpager的监听跳转到tips
+//                img_1.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Intent intent1 = new Intent();
+//                        intent1.setClass(MainActivity.this, TIPS.class);
+//                        startActivity(intent1);
+//                    }
+//                });
+//                img_2.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Intent intent2 = new Intent();
+//                        intent2.setClass(MainActivity.this, TIPS.class);
+//                        startActivity(intent2);
+//                    }
+//                });
+//                img_3.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Intent intent3= new Intent();
+//                        intent3.setClass(MainActivity.this, TIPS.class);
+//                        startActivity(intent3);
+//                    }
+//                });
             }
 
             @Override
@@ -226,13 +266,59 @@ public class MainActivity extends AppCompatActivity implements XScrollView.IXScr
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(MainActivity.this,"hello",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
                 onLoad();
             }
         }, 2500);
     }
+                //需要改监听的对象!!!!!!!!!!!!下面的是联系后台的代码！！！
+//                btn_lookmore.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        new Thread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                Socket socket = null;
+//                                String message = tv_dianliang.getText().toString() + "\r\n";
+//                                try {
+//                                    //创建Socket
+//                                    socket = new Socket("119.29.136.149", 9999); //查看本机IP,每次开机都不同
+//                                    //向服务器发送消息
+//                                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+//                                    out.println(1);
+//
+//                                    //接收来自服务器的消息
+//                                    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                                    String msg = br.readLine();
+//
+//                                    if (msg != null) {
+////                                        tv_dianliang.setText(msg);
+//                                        Log.e("content", msg);
+//                                    } else {
+//                                        tv_dianliang.setText("数据错误!");
+//                                    }
+//                                    //关闭流
+//                                    out.close();
+//                                    br.close();
+//                                    //关闭Socket
+//                                    socket.close();
+//                                } catch (Exception e) {
+//                                    // TODO: handle exception
+//                                    Log.e("error", e.toString());
+//                                }
+//                            }
+//                        }).start();
+//
+//                    }
+//                });
+//                onLoad();
+//            }
+//        }, 2500);
 
-    @Override
+
+
+
+
     public void onLoadMore() {
 
     }
@@ -249,7 +335,6 @@ public class MainActivity extends AppCompatActivity implements XScrollView.IXScr
     }
 
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.option_menu, menu);
--- app/src/main/java/com/baidumap/test/tap/widget/lishiyongshui_activity.java
+++ app/src/main/java/com/baidumap/test/tap/widget/lishiyongshui_activity.java
@@ -1,11 +1,13 @@
 package com.baidumap.test.tap.widget;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.baidumap.test.tap.R;
 import com.github.mikephil.charting.charts.LineChart;
@@ -20,14 +22,159 @@ public class lishiyongshui_activity extends AppCompatActivity {
     public ArrayList<String> labels = new ArrayList<String>();
     public ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
     public LineDataSet dataset;
+    public TextView tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9,tv10,tv11,tv12,tv13,tv14,tv15,tv16,tv17,tv18,tv19,tv20,tv21,tv22,tv23,tv24,tv25,tv26,tv27,tv28;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_lishiyongshui_activity);
+        initView();
+        initListener();
+
+    }
+    //初始化所有日期view
+    private void initView() {
+        tv1= (TextView) findViewById(R.id.tv1);
+        tv2= (TextView) findViewById(R.id.tv2);
+        tv3= (TextView) findViewById(R.id.tv3);
+        tv4= (TextView) findViewById(R.id.tv4);
+        tv5= (TextView) findViewById(R.id.tv5);
+        tv6= (TextView) findViewById(R.id.tv6);
+        tv7= (TextView) findViewById(R.id.tv7);
+        tv8= (TextView) findViewById(R.id.tv8);
+        tv9= (TextView) findViewById(R.id.tv9);
+        tv10= (TextView) findViewById(R.id.tv10);
+        tv11= (TextView) findViewById(R.id.tv11);
+        tv12= (TextView) findViewById(R.id.tv12);
+        tv13= (TextView) findViewById(R.id.tv13);
+        tv14= (TextView) findViewById(R.id.tv14);
+        tv15= (TextView) findViewById(R.id.tv15);
+        tv16= (TextView) findViewById(R.id.tv16);
+        tv17= (TextView) findViewById(R.id.tv17);
+        tv18= (TextView) findViewById(R.id.tv18);
+        tv19= (TextView) findViewById(R.id.tv19);
+        tv20= (TextView) findViewById(R.id.tv20);
+        tv21= (TextView) findViewById(R.id.tv21);
+        tv22= (TextView) findViewById(R.id.tv22);
+        tv23= (TextView) findViewById(R.id.tv23);
+        tv24= (TextView) findViewById(R.id.tv24);
+        tv25= (TextView) findViewById(R.id.tv25);
+        tv26= (TextView) findViewById(R.id.tv26);
+        tv27= (TextView) findViewById(R.id.tv27);
+        tv28= (TextView) findViewById(R.id.tv28);
     }
 
+        //监听日期，实现跳转到当天用水
+    public void initListener(){
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent();
+                intent1.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent1);
+            }
+        });
+
+        tv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3=new Intent();
+                intent3.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent3);
+            }
+        });
+
+        tv7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent7=new Intent();
+                intent7.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent7);
+            }
+        });
+
+        tv9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent9=new Intent();
+                intent9.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent9);
+            }
+        });
+        tv11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent11=new Intent();
+                intent11.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent11);
+            }
+        });
+        tv13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent13=new Intent();
+                intent13.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent13);
+            }
+        });
+        tv15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent15=new Intent();
+                intent15.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent15);
+            }
+        });
+        tv17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent17=new Intent();
+                intent17.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent17);
+            }
+        });
+        tv19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent19=new Intent();
+                intent19.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent19);
+            }
+        });
+        tv21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent21=new Intent();
+                intent21.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent21);
+            }
+        });
+        tv23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent23=new Intent();
+                intent23.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent23);
+            }
+        });
+        tv25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent25=new Intent();
+                intent25.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent25);
+            }
+        });
+        tv27.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent27=new Intent();
+                intent27.setClass(lishiyongshui_activity.this,dangtianyongshui_activity.class);
+                startActivity(intent27);
+            }
+        });
+    }
     private void  show(){
         chart.setDescription("");
 
